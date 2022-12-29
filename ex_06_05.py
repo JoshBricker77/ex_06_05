a637e77bc71ff3a46564b474753e67de5e086fa6
Joshua Bricker <joshbricker77@gmail.com>
2022-12-29 10:11:40 -0500

    Add files via upload

---
+++ ex_06_05.py
@@ -0,0 +1,12 @@
+str = 'x-DSPAM-Confidence: 0.8475'
+
+#string parcing
+
+ipos = str.find(':')
+#print(ipos)
+piece = str[ipos+2:]
+print(piece)
+#print(piece+42.0) #will fail
+value = float(piece)
+print(value)
+#print(value+42.0)
